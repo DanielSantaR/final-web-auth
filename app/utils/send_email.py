bf2e72d7ddd7511ae833435c1f140aa1f3c918ab
Daniel Santa <daniel.santar@udea.edu.co>
2020-11-21 16:00:03 -0500

    Add atributes to email template

--- app/utils/send_email.py
+++ app/utils/send_email.py
@@ -1,19 +1,18 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from typing import Union
 
 from app.core.config import Settings, get_settings
 
 settings: Settings = get_settings()
 
 
-def send_email(*, email_to: str, subject: str, message: str) -> bool:
+def send_email(*, email_to: str, subject: str, message: Union[str, MIMEText]) -> bool:
     try:
         msg = MIMEMultipart()
-        message = message
         # create message object instance
         msg = MIMEMultipart()
-        message = message
         # setup the parameters of the message
         password = settings.SMTP_PASSWORD
         msg["From"] = settings.SMTP_USER
@@ -34,17 +33,27 @@ def send_email(*, email_to: str, subject: str, message: str) -> bool:
         print(e)
         return False
 
+
 def send_code_email(*, email_to: str, code: str):
     with open("./app/email_templates/send_code.html") as f:
         template_str = f.read()
     subject = "Security Code"
-    message = MIMEText(template_str.format(code=code), 'html')
-    send_email(email_to = email_to, subject = subject, message = message)
+    message = MIMEText(template_str.format(code=code), "html")
+    flag = send_email(email_to=email_to, subject=subject, message=message)
+    return flag
 
 
 def send_new_account(*, email_to: str, name: str, username: str):
     with open("./app/email_templates/new_account.html") as f:
         template_str = f.read()
     subject = "Thanks for Create a new Account"
-    message = MIMEText(template_str.format(name=name, username=username), 'html')
-    send_email(email_to = email_to, subject = subject, message = message)
+    message = MIMEText(
+        template_str.format(
+            name=name,
+            username=username,
+            password="Ask for the password to the assistant",
+        ),
+        "html",
+    )
+    flag = send_email(email_to=email_to, subject=subject, message=message)
+    return flag
