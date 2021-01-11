7c629c11e3861b0025ec5cb416f322704119585d
Daniel <daniel.torresg@udea.edu.co>
2021-01-11 16:05:52 -0500

    adding email templates and send email function

--- app/utils/send_email.py
+++ app/utils/send_email.py
@@ -57,3 +57,78 @@ def send_new_account(*, email_to: str, name: str, username: str):
     )
     flag = send_email(email_to=email_to, subject=subject, message=message)
     return flag
+
+
+def send_assigned_vehicle(*, email_to: str,  plate: str, brand: str, model: str, color: str, vehicle_type: str):
+    with open("./app/email_templates/assigned_vehicle.html") as f:
+        template_str = f.read()
+    subject = "A vehicle has been assigned"
+    message = MIMEText(
+        template_str.format(            
+            plate=plate,
+            brand=brand,
+            model=model,
+            color=color,
+            vehicle_type=vehicle_type
+        ),
+        "html",
+    )
+    flag = send_email(email_to=email_to, subject=subject, message=message)
+    return flag
+
+    
+def send_updated_personal_information(*, email_to: str):
+    with open("./app/email_templates/updated_personal_information.html") as f:
+        template_str = f.read()
+    subject = "Update Personal Information"
+    message = MIMEText(
+        template_str.format(),"html",
+    )
+    flag = send_email(email_to=email_to, subject=subject, message=message)
+    return flag
+
+
+    
+def send_reparation_detail(*, email_to: str,  description: str, cost: float):
+    
+    with open("./app/email_templates/reparation_detail.html") as f:
+        template_str = f.read()
+    subject = "New Reparation Detail"
+    message = MIMEText(
+        template_str.format(
+            description=description, 
+            cost=cost
+        ),
+        "html",
+    )
+    flag = send_email(email_to=email_to, subject=subject, message=message)
+    return flag
+
+
+def send_new_owner(*, email_to: str, identity_card: str, name: str, surname: str, phone: str):
+    with open("./app/email_templates/new_owner.html") as f:
+        template_str = f.read()
+    subject = "New account created successfully"
+    message = MIMEText(
+        template_str.format(
+            identity_card=identity_card,
+            name=name,
+            surname=surname,
+            phone=phone,
+            email=email_to
+        ),
+        "html",
+    )
+    flag = send_email(email_to=email_to, subject=subject, message=message)
+    return flag
+
+    
+def send_updated_vehicle(*, email_to: str):
+    with open("./app/email_templates/updated_vehicle.html") as f:
+        template_str = f.read()
+    subject = "Update Vehicle Information"
+    message = MIMEText(
+        template_str.format(),"html",
+    )
+    flag = send_email(email_to=email_to, subject=subject, message=message)
+    return flag
